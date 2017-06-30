0a2c93d0296a1064effb4d495cf114b0accf79b1
maxeskell <maxeskell1@gmail.com>
2017-06-30 11:32:58 +0100

    show route complete

--- src/js/controllers/trails.js
+++ src/js/controllers/trails.js
@@ -1,6 +1,7 @@
 angular
   .module('wildside')
-  .controller('TrailsIndexCtrl', TrailsIndexCtrl);
+  .controller('TrailsIndexCtrl', TrailsIndexCtrl)
+  .controller('TrailsShowCtrl', TrailsShowCtrl);
 
 TrailsIndexCtrl.$inject = ['Trail'];
 
@@ -8,4 +9,20 @@ function TrailsIndexCtrl(Trail) {
   const vm = this;
 
   vm.all = Trail.query();
+}
+
+
+TrailsShowCtrl.$inject = ['Trail', '$state'];
+
+function TrailsShowCtrl(Trail, $state) {
+  const vm = this;
+  vm.trail = Trail.get($state.params);
+
+  function trailsDelete() {
+    vm.trail
+      .$remove()
+      .then(() => $state.go('trailsIndex'));
+  }
+
+  vm.delete = trailsDelete;
 }
