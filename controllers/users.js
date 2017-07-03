741f4a4f38fd9913f54356d5d87ac6497b44f687
maxeskell <maxeskell1@gmail.com>
2017-07-03 14:15:31 +0100

    user routes fixed

--- controllers/users.js
+++ controllers/users.js
@@ -1,17 +1,53 @@
 const User = require('../models/user');
 
-function showRoute(req, res, next) {
+// show user profile
+function userShow(req, res, next) {
   User
     .findById(req.params.id)
     .exec()
     .then((user) => {
-      if(!user) return res.notFound();
+      if (!user) return res.notFound();
 
       res.json(user);
     })
     .catch(next);
 }
 
+
+function userUpdate(req, res, next) {
+  if (req.file) req.body.image = req.file.key;
+
+  User
+    .findById(req.params.id)
+    .exec()
+    .then((user) => {
+      if (!user) return res.notFound();
+
+      for (const field in req.body) {
+        user[field] = req.body[field];
+      }
+
+      return user.save();
+    })
+    .then((user) => res.json(user))
+    .catch(next);
+}
+
+function userDelete(req, res, next) {
+  User
+    .findById(req.params.id)
+    .exec()
+    .then((user) => {
+      if (!user) return res.notFound();
+
+      return user.remove();
+    })
+    .then(() => res.status(204).end())
+    .catch(next);
+}
+
 module.exports = {
-  show: showRoute
+  show: userShow,
+  update: userUpdate,
+  delete: userDelete
 };
