c8821a6f8a16bb8c9d545af8d2c2cee483387d42
Ollymid <ollymiddleton@hotmail.co.uk>
2017-07-03 10:31:46 +0100

    added maps custom directive to use in show page

---
+++ src/js/directives/googleMaps.js
@@ -0,0 +1,42 @@
+/* global google */
+angular
+      .module('wildside')
+      .directive('googleMap', googleMap);
+
+function googleMap() {
+  return {
+    restrict: 'E',
+    replace: true,
+    template: '<div class="map">GOOGLE MAP HERE</div>',
+    scope: {
+      center: '='
+    },
+    link(scope, element) {
+
+      let map = null;
+      let marker = null;
+
+      scope.$watch('center', initMap);
+      scope.$on('$destroy', destroyMap);
+      function initMap(center) {
+        if(!center) return false;
+        map = new google.maps.Map(element[0], {
+          zoom: 14,
+          center: center
+        });
+
+        marker = new google.maps.Marker({
+          position: center,
+          map
+        });
+      }
+
+      function destroyMap() {
+        console.log('bye map');
+        marker.setMap(null);
+        marker = null;
+        map = null;
+      }
+    }
+  };
+}
