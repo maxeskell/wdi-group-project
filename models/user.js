52702f1b75db76fe5157135b768646f7b757de51
maxeskell <maxeskell1@gmail.com>
2017-07-06 15:58:00 +0100

    validation and seeds updated

--- models/user.js
+++ models/user.js
@@ -8,7 +8,6 @@ const userSchema = new mongoose.Schema({
     type: String,
     required: true,
     trim: true,
-    unique: true,
     match: /^[A-Za-z0-9]+(?:[ _-][A-Za-z0-9]+)*$/
   },
   email: {
